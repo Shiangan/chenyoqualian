4f0d1c69ba45d1489b1d90ce5beadc3642aadafa
Shiangan <avarubby85110@gmail.com>
2024-08-14 10:25:03 +0800

    Create Comment.js

---
+++ model/Comment.js
@@ -0,0 +1,10 @@
+// models/Comment.js
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    content: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
