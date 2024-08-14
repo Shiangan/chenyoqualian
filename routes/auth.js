2e2360b1c920c6a05912f28f9eb35e317507320e
Shiangan <avarubby85110@gmail.com>
2024-08-14 10:26:01 +0800

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+// routes/auth.js
+const express = require('express');
+const passport = require('passport');
+const User = require('../models/User');
+
+const router = express.Router();
+
+// 用户注册路由
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = new User({ username, password });
+        await user.save();
+        res.status(201).send('用户注册成功');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+// 用户登录路由
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/comments',
+    failureRedirect: '/login'
+}));
+
+module.exports = router;
